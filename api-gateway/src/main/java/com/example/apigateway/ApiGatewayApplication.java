c2b68973ac1d0057a3584a62fb13a4f39b30ab9f
HyJan <hejian@52tt.com>
2020-07-29 10:26:04 +0800

    update

--- api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -3,6 +3,9 @@ package com.example.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * https://juejin.im/post/5db6eed6518825644076d0b6
+ */
 @SpringBootApplication
 public class ApiGatewayApplication {
 
